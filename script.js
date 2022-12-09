ace5a57ada394ef118f8b58de07247ed89cecf44
Feyza Basaran <basa0044@algonquinlive.com>
2022-12-09 04:18:57 -0500

    favorites page created and trash icons added

--- script.js
+++ script.js
@@ -5,10 +5,12 @@
  const $container = document.getElementById('container')
  const $bimg = document.getElementById('background-img')
  const $image = document.querySelector('.content-img')
+ const $favs = document.getElementById('favs')
 
 let contents = {}
+let favorites = []
 
-function showContent(){
+function showContent(contents){
     const html = []
 
         html.push( /*html*/`
@@ -22,7 +24,7 @@ function showContent(){
             <div class="content-date">
                 <div><i class="fa-solid fa-clock"></i>></div>
                 <p>${contents.date}</p>
-                <i class="fa-solid fa-heart"></i>
+                <i class="fav fa-solid fa-heart" data-url="${contents.url}" data-alt="${contents.title}"></i>
             </div>
     
         </div>`)
@@ -32,24 +34,72 @@ function showContent(){
     }
 
 
+
+
 const date = `${$date.value}`
 
 $form.addEventListener('submit', async function getContent(date){
     date.preventDefault()
     const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=rvEOr6VJE05Ly4t5QqDewZKQMndl3a91eKSjT0Ke&date=' + `${$date.value}`)
         contents = await response.json()
-        showContent()
+        showContent(contents)
+        $favs.innerHTML = ''
 })
+
+function showFav(){
+    const fav = localStorage.getItem('fav')
+    if(fav){
+        favorites = JSON.parse(fav)
+    }
+
+    const html = []
+
+    html.push(`<h2>FAVORITES</h2>`)
+    for(let item of favorites)
+    html.push( /*html*/`
+       <div><img src="${item.url}" alt="${item.alt}"><i class="fa-solid fa-trash"></i></div>`)
+
+    $favs.innerHTML = html.join('')
+    $contents.innerHTML = ''
+}
 
 
 
 $contents.addEventListener('click', function(e){
-    
-    const html = []
+    e.preventDefault()
     if(e.target.classList.contains('content-img')){
-    $contents.innerHTML = `<div class='content' ><img class="content-img" src="${contents.hdurl}" alt="${contents.title}"></div>`
+    $contents.innerHTML = `<div class='content' ><img class="hdimg" src="${contents.hdurl}" alt="${contents.title}">                
+    <i class="back fa-solid fa-arrow-left"></i></div>`
+    }else if(e.target.classList.contains('back')){
+        showContent(contents)
+    }else if(e.target.classList.contains('fav')){
+        favorites.push({
+            url: e.target.dataset.url,
+            alt: e.target.dataset.alt
+        })
+        localStorage.setItem('fav', JSON.stringify(favorites))
     }
 })
+
+$favorites.addEventListener('click', function(e){
+    e.preventDefault()
+    showFav()
+})
+
+$favs.addEventListener('click', function(e){
+    e.preventDefault()
+    if(e.target.classList.contains('fa-trash')){
+        const url = e.target.dataset.index
+        favorites.splice(index, 1)
+        localStorage.setItem('fav', JSON.stringify(favorites))
+        showFav()
+    }
+})
+
+
+
+
+
 
 
 
